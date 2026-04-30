8773c5aa5660c266f835edc1f3c8303ccc738824
Misterbck <moisev@cloudsmonster.com>
2026-04-30 20:36:22 +0200

    fixed the error on the next.config file

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
-// @ts-ignore
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-=======
-  /* config options here */
->>>>>>> 9bf917f (The initial settings of the Project, until now we did the set-up, creation of the files, sign-in & sign-up design done, we have also the integration of Imagekit on the Project)
   images: {
     remotePatterns: [
       {
@@ -19,22 +14,13 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "ik.imagekit.io",
-<<<<<<< HEAD
+        port: "",
       },
     ],
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-=======
-        port: "",
-      },
-    ],
-  },
->>>>>>> 9bf917f (The initial settings of the Project, until now we did the set-up, creation of the files, sign-in & sign-up design done, we have also the integration of Imagekit on the Project)
+  }
 };
 
 export default nextConfig;
